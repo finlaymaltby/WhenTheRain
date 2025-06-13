3b9907d1c5f843a0142b54631f754565f535d914
finlaymaltby <finlay3.141@gmail.com>
2025-06-13 17:14:50 +1000

    began styling

--- Scripts/Components/health_display.gd
+++ Scripts/Components/health_display.gd
@@ -3,8 +3,7 @@ class_name HealthDisplay
 
 @export var character: CombatBody
 
-func _ready() -> void:
-	label_settings = preload("res://Resources/health_display_font.tres")
+
 func _process(delta: float) -> void:
 	if character:
 		text = str(round(character.health.current_health)) + "/" + str(round(character.health.max_health))
