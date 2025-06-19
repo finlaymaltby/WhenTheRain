d693481d34c14920d375530cfcae935a4ac162f9
finlaymaltby <finlay3.141@gmail.com>
2025-06-20 02:19:08 +1000

    :)

--- areas/demo/demo.gd
+++ areas/demo/demo.gd
@@ -1,4 +1,4 @@
 extends LocalStory
 
 func demo_fight() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Fights/side_on.tscn")
+	get_tree().change_scene_to_file("res://fights/demo_fight/demo_fight.tscn")
