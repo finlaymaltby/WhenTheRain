e110abf04180250e2aebfd99ad70eb2a87920b32
finlaymaltby <finlay3.141@gmail.com>
2025-06-29 17:04:50 +1000

    (possibly) working demo for dialogue

--- areas/demo/demo.gd
+++ areas/demo/demo.gd
@@ -1,9 +1,5 @@
 class_name Demo extends LocalStory
 
-var local_var := 0:
-	set(value):
-		breakpoint
-
 
 func demo_fight() -> void:
 	get_tree().change_scene_to_file("res://fights/demo_fight/demo_fight.tscn")
